6506862e5813f56888625da89115bfc0d7b1c06c
prakash22-26 <prakashkumarshah156@gmail.com>
2026-07-30 14:15:54 +0530

    Update transcription_service.py

--- backend/services/transcription_service.py
+++ backend/services/transcription_service.py
@@ -9,8 +9,9 @@ from moviepy import VideoFileClip
 # LOAD MODEL
 # -----------------------------------
 
-client = Groq(
-    api_key=os.getenv("GROQ_API_KEY")
+client = OpenAI(
+    api_key=os.getenv("GROQ_API_KEY"),
+    base_url="https://api.groq.com/openai/v1"
 )
 
 
